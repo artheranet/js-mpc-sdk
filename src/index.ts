cb03ebc1fbc7368a8cf28d4098b4d083e2f06637
flaviusburca <flaviusburca@users.noreply.github.com>
2024-04-22 10:03:29 +0300

    Export specific items only

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './login';
-export * from './signer';
-export * from './constants';
+export {ArtheraLogin, AuthMethod} from './login';
+export {ArtheraSigner} from './signer';
+export {MPC_AUTH_ENDPOINT, ARTHERA_NETWORK_DETAILS, ARTHERA_TESTNET_ID, ARTHERA_MAINNET_ID} from './constants';
